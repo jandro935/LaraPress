dc503e09bbe85e6a41907821615e313972aaf28e
jandro935 <jandro935@gmail.com>
2017-09-21 10:22:56 +0200

    Small Changes

--- laravel/app/Http/Controllers/PostsController.php
+++ laravel/app/Http/Controllers/PostsController.php
@@ -8,7 +8,6 @@ class PostsController extends Controller
 {
 	public function index()
 	{
-//		$data = Posts::all(); --> All posts (published or not)
 //		$data = Posts::published()->get();
 
 		$data = Posts::type('post')->published()->paginate(2);
@@ -18,7 +17,6 @@ class PostsController extends Controller
 
 	public function show($slug)
 	{
-//		$data = Posts::find($id);
 		$data = Posts::where('post_name', '=', $slug)->firstOrFail();
 
 		return view('posts/details', compact('data'));
